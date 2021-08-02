7bfca05e7588a6769843348a442481f053834765
JAFB321 <jafb321@gmail.com>
2021-08-02 10:37:25 -0700

    feat: init and get gameboard state function

--- src/game/GameLife.js
+++ src/game/GameLife.js
@@ -1,5 +1,19 @@
-import {GameBoard} from './GameBoard'
+import { GameBoard, Point } from "./GameBoard";
 
 export class GameLife {
     
+    gameBoard;
+
+    constructor(){
+        this.gameBoard = new GameBoard();
+    }
+
+    initGame(initialCells = [[0,0]]){
+        initialCells.forEach(([x,y]) => this.gameBoard.setCell(x, y, true));
+    }
+
+    getGameboard(size = [100, 100]){
+        
+    }
+
 }
