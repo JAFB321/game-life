ed008d2742344e712497e1cb3610487217abe8a0
JAFB321 <jafb321@gmail.com>
2021-08-02 22:29:44 -0700

    fix: getBoard variables name

--- src/game/GameBoard.js
+++ src/game/GameBoard.js
@@ -123,18 +123,18 @@ export class GameBoard {
     }
 
     getBoard(size = new Rectangle(-100, -100, 100, 100)){
-        const newPlane = new CartesianPlane(false);
+        const plane = new CartesianPlane(false);
 
         for(let x_pos = size.point1.x; x_pos < size.point2.x; x_pos++){
             for(let y_pos = size.point1.y; y_pos < size.point2.y; y_pos++){
                 const point = new Point(x_pos, y_pos);
                 const pointValue = this.board.getPoint(point);
-                if(!!pointValue) newPlane.setPoint(point, pointValue);
+                if(!!pointValue) plane.setPoint(point, pointValue);
             }
         }
 
         return{
-            board: newPlane,
+            board: plane,
             size: size
         }
     }
