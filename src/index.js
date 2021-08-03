883daa0eed8c23ddb42ff0cdda5ada0fbac8ed82
JAFB321 <jafb321@gmail.com>
2021-08-03 01:29:14 -0700

    feat: tests cells grow

--- src/index.js
+++ src/index.js
@@ -17,17 +17,27 @@ window.game = new GameBoard();
 var game = window.game;
 
 // game.setCell(new Point(100,100));
-game.setCell(new Point(100,101));
-game.setCell(new Point(101,100));
-game.setCell(new Point(101,101));
-game.setCell(new Point(-100,-100));
-game.setCell(new Point(50,-50));
-game.setCell(new Point(-100,100));
+// game.setCell(new Point(50,50));
+// game.setCell(new Point(51,50));
+// game.setCell(new Point(52,50));
+// game.setCell(new Point(51, 49));
+
+game.setCell(new Point(50,50));
+game.setCell(new Point(51,51));
+game.setCell(new Point(51,52));
+game.setCell(new Point(50,52));
+game.setCell(new Point(49,52));
+
+// game.setCell(new Point(50,50));
+// game.setCell(new Point(51,51));
+// game.setCell(new Point(52,51));
+// game.setCell(new Point(51,52));
+// game.setCell(new Point(50,52));
 
 window.printBoard = () =>{
     for (let x = 0; x < 202; x++) {
         for (let y = 0; y < 202; y++) {
-            const isAlive = game.getCell(new Point(x-100, y-100));
+            const isAlive = game.getCell(x-100, y-100);
     
             if(isAlive){
                 ctx.fillRect(x+x, y+y, 1, 1)
@@ -40,3 +50,14 @@ window.cleanBoard = () => {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
 }
 
+window.printBoard();
+
+window.nextGen = () => {
+    window.cleanBoard();
+    game.nextGeneration();
+    window.printBoard();
+}
+
+setInterval(() => {
+    window.nextGen();
+}, 100);
