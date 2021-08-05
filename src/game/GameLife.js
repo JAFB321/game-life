2a7d3f0f3e30da44f83ee1adc391303d7e62fe24
JAFB321 <jafb321@gmail.com>
2021-08-04 17:54:07 -0700

    feat: evolution control functions

--- src/game/GameLife.js
+++ src/game/GameLife.js
@@ -4,6 +4,16 @@ export class GameLife {
 
     gameBoard;
 
+    _evolution = {
+        isEvolving: false,
+        intervalID: -1,
+        config: {
+            onNextGeneration: (size = new Rectangle(0,0,100,100), board = new CartesianPlane(false)) => {},
+            delayDuration: 200,
+            size = new Rectangle(0,0,100,100)
+        }
+    }
+
     constructor(){
         this.gameBoard = new GameBoard();
     }
@@ -16,16 +26,65 @@ export class GameLife {
         this.gameBoard.setCell(x, y, false);
     }
 
+    exterminateCells(){
+        this.gameBoard.resetCells();
+    }
+
     getBoard(size = new Rectangle(-100, -100, 100, 100)){
         return this.gameBoard.getBoard(size);
     }
 
-    initGame(options = {
-         onNextGeneration: (board, size) => {},
-         timeInterval: 1000,
-         
-        }){
+    startEvolution(options = {
+        onNextGeneration: (size = new Rectangle(0,0,100,100), board = new CartesianPlane(false)) => {},
+        delayDuration: 200,
+        size = new Rectangle(0,0,100,100)
+    }){
+        const { onNextGeneration, delayDuration, size } = options;
+        const { isEvolving } = this._evolution;
         
+        if(isEvolving){
+            this.pauseEvolution();
+        }
+        
+        const intervalID = setInterval(() => {
+            this.gameBoard.nextGeneration();
+            const { board } = this.gameBoard.getBoard(size);
+
+            onNextGeneration(size, board);
+        }, delayDuration);
+
+        this._evolution.isEvolving = true;
+        this._evolution.intervalID = intervalID;
+
+
+    }
+
+    pauseEvolution(){
+        const { isEvolving, intervalID } = this._evolution;
+         
+        if(isEvolving && intervalID !== -1){
+            clearInterval(intervalID);
+            this._evolution.intervalID = -1;
+            this._evolution.isEvolving = false;
+        }
+    }
+
+    resumeEvolution(){
+        const { isEvolving, config } = this._evolution;
+        
+        if(!isEvolving){
+            const { delayDuration, onNextGeneration, size } = config;
+            
+            const intervalID = setInterval(() => {
+                this.gameBoard.nextGeneration();
+                const { board } = this.gameBoard.getBoard(size);
+
+                onNextGeneration(size, board);
+            }, delayDuration);
+
+            this._evolution.isEvolving = true;
+            this._evolution.intervalID = intervalID;
+        }
     }
 
 
