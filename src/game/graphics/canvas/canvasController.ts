851f1306a11f14a9d8b0ae0c8e3deb55f758de9e
jafb321 <jafb321@gmail.com>
2022-03-24 19:04:03 -0700

    refactor: Canvas graphics controller

--- src/game/graphics/canvas/canvasController.ts
+++ src/game/graphics/canvas/canvasController.ts
@@ -1,56 +1,21 @@
-import { CartesianPlane } from "../../structures/CartesianPlane";
+import { GameBoard } from "../../core/GameBoard.js";
+import { GraphicsController } from "../GraphicsController.js";
 
-export interface GraphicsConfig {
-    cells: {
-        size: number,
-    },
-    grid: {
-        lineWidth: number,
-    }
-    board: {
-        height: number,
-        width: number,
-    },
-    colors: {
-        background: string,
-        cell: string,
-        grid: string
-    }
-}
-
-export class CanvasController{
+export class CanvasController extends GraphicsController {
 
     private canvas: HTMLCanvasElement;
     private canvasContext: CanvasRenderingContext2D | null;
-    private config: GraphicsConfig;
 
     constructor(canvas: HTMLCanvasElement){
+        super();
         this.canvas = canvas;
         this.canvasContext = canvas?.getContext("2d");
 
         if(!this.canvas || !this.canvasContext)
           throw new Error("Canvas cannot be null");
-
-        this.config = {
-            cells: {
-                size: 18,
-            },
-            grid: {
-                lineWidth: 0.8,
-            },
-            board: {
-                height: canvas.height,
-                width: canvas.width,
-            },
-            colors: {
-                background: '#22272e',
-                cell: '#ffffff',
-                grid: '626567'
-            }
-        }
     }
 
-    public render(board: CartesianPlane<boolean>){
+    public render(board: GameBoard){
         
     }
 
